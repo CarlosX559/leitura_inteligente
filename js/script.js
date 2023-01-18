8ffb61a69ce01efd37eac9c092ba2109a4003ade
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-01-18 15:47:36 -0300

    section faq

--- js/script.js
+++ js/script.js
@@ -3,3 +3,29 @@
     duration: 600
    
 });*/
+
+function faq() {
+
+    const pergunta = document.querySelectorAll(".faq");
+    const resposta = document.querySelectorAll(".faq_text");
+
+    for (let i = 0; i < pergunta.length; i++) {
+
+        pergunta[i].addEventListener("click", () => {
+
+
+            if (pergunta[i].classList.contains('fechar')) {
+                pergunta[i].classList.toggle('fechar');
+                resposta[i].classList.toggle('ativar');
+            } else {
+                pergunta[i].classList.add('fechar');
+                resposta[i].classList.add('ativar');
+            }
+
+        });
+
+    }
+
+}
+
+window.addEventListener('load', faq());
