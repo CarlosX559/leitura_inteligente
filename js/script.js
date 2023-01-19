b196bc34c666b7beb3abbe736ea0cd5b78f293c1
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-01-19 11:42:32 -0300

    add animations scroll

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-/*new Vivus('bg_home', {
+new Vivus('bg_home', {
 
     duration: 600
    
-});*/
+});
 
 function faq() {
 
@@ -29,3 +29,24 @@ function faq() {
 }
 
 window.addEventListener('load', faq());
+
+const animations = document.querySelectorAll("[data-animation]");
+const animationClass = "animate";
+
+function animation_scroll() {
+  const area_window = window.innerHeight * 0.21 * 3.8;
+
+  animations.forEach((element) => {
+    let posicaoAtual = element.getBoundingClientRect().top;
+
+    if (area_window > posicaoAtual) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
+}
+
+if (animations.length) {
+  window.addEventListener("scroll", animation_scroll);
+}
